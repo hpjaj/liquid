300f2af5970c04a87277b617056c35e026a94917
hpjaj <hmlevine@gmail.com>
2014-10-26 20:23:56 -0600

    Adding Liquid Mechanics Brewery site to GitHub repo

---
+++ imageSlide2.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#slide1_controls').on('click', 'span', function(){
+    $("#slide1_images").css("transform","translateX("+$(this).index() * -898+"px)");
+    $("#slide1_controls span").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
+
+
+$( "ul li a" ).mouseenter(function() {
+  $( this ).switchClass( "noHover", "hover", 200, "easeInOutQuad" );
+});
+
+$( "ul li a" ).mouseleave(function() {
+  $( this ).switchClass( "hover", "noHover", 200, "easeInOutQuad" );
+});
+
+
